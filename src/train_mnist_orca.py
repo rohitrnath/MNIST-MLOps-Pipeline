6a2cd5e28082c5297f71ba8db47a953f9f3d6ae5
rohitrnath <rohitpathanamthitta@gmail.com>
2025-09-11 22:27:30 +0530

    Add global average pooling(GAP)

--- src/train_mnist_orca.py
+++ src/train_mnist_orca.py
@@ -19,24 +19,24 @@ import mlflow.pytorch as mlflow_pytorch
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # Input Block
+        # Input Block 28  >>> 64
         self.convblock1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
             nn.BatchNorm2d(10),
             nn.ReLU()
-        ) # output_size = 26
+        ) # output_size = 26 >>> 62
 
         # CONVOLUTION BLOCK 1
         self.convblock2 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
             nn.BatchNorm2d(10),
             nn.ReLU()
-        ) # output_size = 24
+        ) # output_size = 24. >>> 60
         self.convblock3 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
             nn.BatchNorm2d(20),
             nn.ReLU()
-        ) # output_size = 22
+        ) # output_size = 22 >>> 58
 
         # TRANSITION BLOCK 1
         self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
@@ -44,30 +44,28 @@ class Net(nn.Module):
             nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
             nn.BatchNorm2d(10),
             nn.ReLU()
-        ) # output_size = 11
+        ) # output_size = 11 >>> 29
 
         # CONVOLUTION BLOCK 2
         self.convblock5 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
             nn.BatchNorm2d(10),
             nn.ReLU()
-        ) # output_size = 9
+        ) # output_size = 9 >>> 27
         self.convblock6 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
             nn.BatchNorm2d(20),
             nn.ReLU()
-        ) # output_size = 7
+        ) # output_size = 7 >>> 25
 
         # OUTPUT BLOCK
         self.convblock7 = nn.Sequential(
             nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
             nn.BatchNorm2d(10),
             nn.ReLU()
-        ) # output_size = 7
-        self.convblock8 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(7, 7), padding=0, bias=False),
-            # nn.BatchNorm2d(10), NEVER
-            # nn.ReLU() NEVER!
+        ) # output_size = 7 >>> 25
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=7) # 7>> 9... nn.AdaptiveAvgPool((1, 1))
         ) # output_size = 1
 
         self.dropout = nn.Dropout(0.25)
@@ -83,7 +81,7 @@ class Net(nn.Module):
         x = self.convblock6(x)
         x = self.dropout(x)
         x = self.convblock7(x)
-        x = self.convblock8(x)
+        x = self.gap(x)
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
 
