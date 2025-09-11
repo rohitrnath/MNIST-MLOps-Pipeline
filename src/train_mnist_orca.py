e881ba9271be67e7b3d6c6c7222241b8d949a766
rohitrnath <rohitpathanamthitta@gmail.com>
2025-09-11 22:13:27 +0530

    Adding batch normalization

--- src/train_mnist_orca.py
+++ src/train_mnist_orca.py
@@ -22,20 +22,19 @@ class Net(nn.Module):
         # Input Block
         self.convblock1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
-            
+            nn.BatchNorm2d(10),
             nn.ReLU()
         ) # output_size = 26
 
         # CONVOLUTION BLOCK 1
         self.convblock2 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
-            
+            nn.BatchNorm2d(10),
             nn.ReLU()
         ) # output_size = 24
         self.convblock3 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
-            
-            
+            nn.BatchNorm2d(20),
             nn.ReLU()
         ) # output_size = 22
 
@@ -43,24 +42,26 @@ class Net(nn.Module):
         self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
         self.convblock4 = nn.Sequential(
             nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.BatchNorm2d(10),
             nn.ReLU()
         ) # output_size = 11
 
         # CONVOLUTION BLOCK 2
         self.convblock5 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
-            
-            
+            nn.BatchNorm2d(10),
             nn.ReLU()
         ) # output_size = 9
         self.convblock6 = nn.Sequential(
             nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
+            nn.BatchNorm2d(20),
             nn.ReLU()
         ) # output_size = 7
 
         # OUTPUT BLOCK
         self.convblock7 = nn.Sequential(
             nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.BatchNorm2d(10),
             nn.ReLU()
         ) # output_size = 7
         self.convblock8 = nn.Sequential(
